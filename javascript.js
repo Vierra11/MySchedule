9be8a6f55e5a0fe5bc3bbf461f30071c019ec6e2
Vierra11 <vrishti.godhwani@gmail.com>
2023-12-26 11:29:36 +0530

    final commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+window.onload = function mode (){
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', (e) => {
+	const isChecked = e.target.checked;
+	
+	if(isChecked) {
+		body.classList.add('dark-theme');
+			
+	} 
+	else {
+		body.classList.remove('dark-theme');
+		
+	}
+});
+}
